67e809c2865dd6cc771dc37c76e17926781923d0
NANDINI3112 <111289426+NANDINI3112@users.noreply.github.com>
2024-07-21 21:26:27 +0530

    Delete Backend/models directory

--- Backend/models/Review.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ReviewSchema = new mongoose.Schema({
-  audiobookId: mongoose.Schema.Types.ObjectId,
-  rating: Number,
-  comment: String,
-});
-
-module.exports = mongoose.model('Review', ReviewSchema);
