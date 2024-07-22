dbb4cf12ecbe992af897fae4e3111eac21125743
NANDINI3112 <111289426+NANDINI3112@users.noreply.github.com>
2024-07-22 09:48:19 +0530

    Update App.js

--- Frontend/Src/App.js
+++ Frontend/Src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AudiobookList from './components/AudiobookList';
 import AudiobookDetails from './components/AudiobookDetails';
+import './styles/App.css';
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={AudiobookList} />
-        <Route path="/audiobooks/:id" component={AudiobookDetails} />
-      </Switch>
-    </Router>
+    <div className="container mt-5">
+      <h1 className="app-title text-center">Audiobook Review App</h1>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={AudiobookList} />
+          <Route path="/audiobooks/:id" component={AudiobookDetails} />
+        </Switch>
+      </Router>
+    </div>
   );
 };
 
